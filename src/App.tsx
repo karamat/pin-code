f17611b1bb6fb98dc88f0018233aa32d2a2a6e6e
Karamat Ullah <karamatullah46@gmail.com>
2023-01-17 13:29:04 +0500

    add input type and value for the input

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import Input from './components/Input';
+import { InputType } from './types';
 import './App.css';
 
 function App() {
   const [pinLength, setPinLength] = useState(5);
+  const [values, setValues] = useState<{ [key: string]: InputType }>({});
+
   return (
     <div className='App'>
       <p>Please enter pin code</p>
       <div className='input-list'>
         {Array.from(Array(pinLength).keys()).map((_, index) => (
-          <Input index={index} />
+          <Input index={index} value={values[index]} />
         ))}
       </div>
     </div>
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { InputType } from './../../types';
 import './styles.css';
 
 interface Props {
   index: number;
+  value: InputType;
 }
 
-const Input: React.FC<Props> = ({ index }) => {
+const Input: React.FC<Props> = ({ index, value }) => {
   return (
     <input
       className='input'
       name={`pin-number-${index}`}
       maxLength={1}
       autoComplete={'nope'}
+      value={value}
     />
   );
 };
