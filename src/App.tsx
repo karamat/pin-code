ae419e2b14b1750a04cf1902ac99186eb6990fd7
Karamat Ullah <karamatullah46@gmail.com>
2023-01-17 12:58:38 +0500

    add empty input compoent and render list on input components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from './components/Input';
 import './App.css';
 
 function App() {
+  const [pinLength, setPinLength] = useState(5);
   return (
-    <div className="App">
-
+    <div className='App'>
+      <div className='input-list'>
+        {Array.from(Array(pinLength).keys()).map(() => (
+          <Input />
+        ))}
+      </div>
     </div>
   );
 }
